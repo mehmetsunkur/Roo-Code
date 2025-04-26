ec9b5d9d3b1bb0cc9bae37d23ec7ae1aeb8ca971
Mehmet Sunkur <mehmetsunkur@gmail.com>
2025-04-26 14:39:18 +0100

    docker release script

--- aos-vsce-release.sh
+++ aos-vsce-release.sh
@@ -1,12 +1,16 @@
 shopt -s nullglob  # ensures the pattern expands to nothing if no match
-files=(./bin/roo-cline-*.vsix)
+files=(./bin/aos-code-*.vsix)
 if (( ${#files[@]} )); then
   echo "File exists: ${files[0]}"
+  the_file_path="${files[0]}"
+  the_file=$(basename "$the_file_path")
+  echo "File name: $the_file"
 else
-  echo "Error! No matching file found: './bin/roo-cline-*.vsix'"
+  echo "Error! No matching file found: './bin/raos-code-*.vsix'"
   exit 1
 fi
 cat <<EOF | docker build -t aos/aos-vsce:2 -t aos/aos-vsce:latest -f - .
 FROM aos/aos-vsce:1
 COPY ./bin/ /data
+RUN echo "$the_file" > /data/latest.txt
 EOF
