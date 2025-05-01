a8ca4712755e6a99d54cd60058b3cddcfecf464d
Mehmet Sunkur <mehmetsunkur@gmail.com>
2025-05-01 13:14:10 +0100

    vsix file release script

---
+++ aos-vsce-release.sh
@@ -0,0 +1,18 @@
+## release the current code changes as docker image
+## builds docker image tagged aos/aos-vsce with the vsix file './bin/aos-code-*.vsix'
+shopt -s nullglob  # ensures the pattern expands to nothing if no match
+files=(./bin/aos-code-*.vsix)
+if (( ${#files[@]} )); then
+  echo "File exists: ${files[0]}"
+  the_file_path="${files[0]}"
+  the_file=$(basename "$the_file_path")
+  echo "File name: $the_file"
+else
+  echo "Error! No matching file found: './bin/aos-code-*.vsix'"
+  exit 1
+fi
+cat <<EOF | docker build -t aos/aos-vsce:2 -t aos/aos-vsce:latest -f - .
+FROM aos/aos-vsce:1
+COPY ./bin/ /data
+RUN echo "$the_file" > /data/latest.txt
+EOF
